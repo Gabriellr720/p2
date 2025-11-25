a819f59e3d5b4fba0f5bf5b91f95b7f01ce868a5
Gabriellr720 <glimarocha662@gmail.com>
2025-11-25 08:54:24 -0300

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method= "POST" action="processamento.php">
+        <input type="text" name="nome" placeholder="Nome: ">
+        <input type="text" name="preco" placeholder="Preço : ">
+        <input type="text" name="preco_promo" placeholder="Preço em promoção : ">
+        <button type="submit">Cadastrar</button>
+    </form>
+</body>
+</html>
